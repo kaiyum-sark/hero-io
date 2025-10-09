c199e9766f96a5b61ab5d47cf699e78e98db0a6d
kaiyum sark <kasark2100@gmail.com>
2025-10-09 20:25:23 +0600

    json data changed

--- src/Components/Cards/FeaturedCard.jsx
+++ src/Components/Cards/FeaturedCard.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import ratingIcon from "../../assets/icon-ratings.png";
 import downloadIcon from "../../assets/icon-downloads.png";
 import { Link } from "react-router";
+import { convertedNumber } from "../../Utilis/convertedNumber";
 
 const FeaturedCard = ({ app }) => {
-  const { id, image, companyName, ratingAvg, downloads } = app;
+  const { id, title, image, companyName, ratingAvg, downloads } = app;
   return (
     <div>
       <Link to={`/apps/${id}`} state={app}>
         <div className="card bg-base-100  shadow-sm hover:scale-105 transition ease-in-out">
           <figure className="pt-3 px-3">
-            <img src={image} alt="Shoes" className="rounded-xl w-full" />
+            <img
+              src={image}
+              alt={title}
+              className="rounded-xl w-full max-h-48"
+            />
           </figure>
           <div className="p-3 items-center text-center">
-            <h2 className="card-title">{companyName}</h2>
+            <h2 className="card-title">{title}</h2>
 
             <div className="flex justify-between gap-5">
               <button className="btn bg-[#F1F5E8] text-[#00D390]">
-                <img className="size-5" src={downloadIcon} alt="" /> {downloads}
+                <img className="size-5" src={downloadIcon} alt="" />{" "}
+                {convertedNumber(downloads)}
               </button>
               <button className="btn bg-[#FFF0E1] text-[#FF8811]">
                 <img className="size-5" src={ratingIcon} alt="" /> {ratingAvg}
